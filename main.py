f521973e366269e4f47611e9750e6dc9849c4e12
Jibrilzito <Gabi04@live.dk>
2023-02-28 10:25:58 +0100

    Join not work

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import discord
+from discord.ext import commands
 
+############## SETUP ##############
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='!', intents=intents)
 
 TOK_FILE = "token.txt"
 
@@ -13,22 +15,40 @@ def get_token():
   tokfile.close()
   return token
 
+@client.command()
+async def join(ctx):
+    print("Joined")
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+@client.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
 @client.event
 async def on_ready():
     print("Connected!")
 
-
 @client.event
 async def on_message(message):
-    contents = message.content
-    
-       
-    if contents.startswith("!echo"):
-        
-        rem = contents[5:]
-        reply = "Du sendte: " + rem
+    contents = message.content.lower()
+
+############## HANDYCODE ##############
+
+    if contents == ("!test"):
+        reply = "Meooooow"
         await message.channel.send(reply)
-           
+
+    if contents == ("!synopsis"):
+        reply = "https://teccph-my.sharepoint.com/personal/gabr1353_elev_tec_dk/_layouts/15/doc.aspx?sourcedoc={7cef49c4-849f-4b12-b908-20f03c2a1b52}&action=edit"
+        await message.channel.send(reply)
+
+    if contents == ("!oplæg"):
+        reply = "file:///C:/Users/gabi0/Downloads/Eksamensopl_g_22_23%20(1).pdf"
+        await message.channel.send(reply)
+
+############## MAINCODE ##############
+
 token = get_token()
 client.run(token)
 
