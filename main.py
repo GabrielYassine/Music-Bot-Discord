c35d5f08e1efc389e7d90fd8a775901a324a9b83
Jibrilzito <101795527+Jibrilzito@users.noreply.github.com>
2023-03-02 12:35:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 ############## SETUP ##############
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -15,30 +16,31 @@ def get_token():
   tokfile.close()
   return token
 
-@client.command()
-async def join(ctx):
-    if ctx.author.voice:
-        channel = ctx.author.voice.channel
-        await ctx.send("Joined")
-        await channel.connect()
-    else:
-        await ctx.send("You are not currently in a voice channel.")
-
-@client.command()
-async def leave(ctx):
-    print(ctx.voice_client) # Add this line
-    if ctx.voice_client:
-        await ctx.voice_client.disconnect()
-        await ctx.send("Left")
-    else:
-        await ctx.send("I am not currently in a voice channel.")
-
 @client.event
 async def on_ready():
     print("Connected!")
 
+@client.command(pass_context = True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await ctx.send("Music Bot has joined the voice channel!")
+        await channel.connect()
+    else:
+        await ctx.send("You are not currently in a voice channel.")
+
+@client.command(pass_context = True)
+async def leave(ctx):
+    if (ctx.voice_client): 
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("Music bot has left the voice channel!")
+    else:
+        await ctx.send("I am not currently in a voice channel.")
+
 @client.event
 async def on_message(message):
+    await client.process_commands(message)
+
     if message.content.lower() == ("!test"):
         reply = "Meooooow"
         await message.channel.send(reply)
@@ -50,8 +52,6 @@ async def on_message(message):
     if message.content.lower() == ("!oplæg"):
         reply = "file:///C:/Users/gabi0/Downloads/Eksamensopl_g_22_23%20(1).pdf"
         await message.channel.send(reply)
-
-    await client.process_commands(message)
 
 token = get_token()
 client.run(token)
